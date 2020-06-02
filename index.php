54a185682e6f1e0574be4850d13f2b647e1039d1
Pablo Marcano <pablo.marcano.16@gmail.com>
2020-06-02 15:21:53 -0400

    second commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Elle Style Coach</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.min.css">
+</head>
+<body>
+	<nav>
+		
+	</nav>
+</body>
+</html>
