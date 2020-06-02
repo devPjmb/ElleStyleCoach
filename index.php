67a6a55d39b7fbffadc291b3a6f03f5b0677face
Pablo Marcano <pablo.marcano.16@gmail.com>
2020-06-02 15:31:30 -0400

    third commit

--- index.php
+++ index.php
@@ -11,5 +11,8 @@
 	<nav>
 		
 	</nav>
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
 </html>
